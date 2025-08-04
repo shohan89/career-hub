69a9dac319082a929a60c59cd0b1d21267381e5a
shoh4n <maffin.killer007@gmail.com>
2025-08-04 17:56:03 +0600

    feat: add Footer comp and set it to Root Layout

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
+import Footer from '../components/Footer';
 
 const Root = () => {
     return (
         <div>
             <Outlet />
+            <Footer />
         </div>
     );
 };
