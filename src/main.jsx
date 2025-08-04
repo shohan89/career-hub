7093a070b7841d2fef86f47ab7062415351e1681
shoh4n <maffin.killer007@gmail.com>
2025-08-04 17:24:51 +0600

    feat: setup Router and add nested routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
-import App from './App.jsx'
+import Root from './layouts/Root.jsx'
+import Home from './Pages/Home.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
