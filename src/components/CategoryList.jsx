769d22d3e81aabf136625044e2b1534fba0106af
shoh4n <maffin.killer007@gmail.com>
2025-08-04 19:36:30 +0600

    feat: add categoryList comp

---
+++ src/components/CategoryList.jsx
@@ -0,0 +1,11 @@
+import SectionTittle from './SectionTittle';
+
+const CategoryList = () => {
+    return (
+        <div>
+            <SectionTittle tittle={'Job Category List'} subTittle={'Explore thousands of job opportunities with all the information you need. Its your future'} />
+        </div>
+    );
+};
+
+export default CategoryList;
