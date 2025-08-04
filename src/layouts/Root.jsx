dbcb6eb6d748d596adf941df07f62974aeade4a1
shoh4n <maffin.killer007@gmail.com>
2025-08-04 18:20:57 +0600

    feat: add Nav comp

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import Footer from '../components/Footer';
+import Nav from '../components/Nav';
 
 const Root = () => {
     return (
         <div>
+            <Nav />
             <Outlet />
             <Footer />
         </div>
