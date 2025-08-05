632f1e1a3f0c9d86c28cf9a1c5a3a9c20e396501
shoh4n <maffin.killer007@gmail.com>
2025-08-05 14:20:57 +0600

    fix navbar width issue

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,7 +9,7 @@ const Nav = () => {
         <li><NavLink to='/blogs'>Blogs</NavLink></li>
     </>
     return (
-        <div className="navbar bg-base-100 shadow-sm">
+        <div className="navbar">
             <div className="navbar-start">
                 <div className="dropdown">
                 <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
