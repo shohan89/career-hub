bab4685c256c8b79c2cf8aef662fa2afd9beaafd
shoh4n <maffin.killer007@gmail.com>
2025-08-05 16:28:50 +0600

    feat: add location and salary

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -1,22 +1,26 @@
 import JobTypeButton from "./JobTypeButton";
 
 const JobCard = ({ featuredJob }) => {
-    const { logo, job_tittle, 
+    const { logo, job_title, 
         company_name, remote_or_onsite, 
         location,job_type, salary } = featuredJob;
     return (
-        <div className="card bg-base-100 shadow-sm">
-            <figure>
+        <div className="card bg-base-100 shadow-sm items-start">
+            <figure className="p-6">
                 <img
                 src={logo}
                 alt="Shoes" />
             </figure>
             <div className="card-body">
-                <h2 className="card-title">{job_tittle}</h2>
+                <h2 className="card-title">{job_title}</h2>
                 <p>{company_name}</p>
                 <div className="flex gap-4">
                     <JobTypeButton buttonText={remote_or_onsite} />
                     <JobTypeButton buttonText={job_type} />
+                </div>
+                <div className="flex">
+                    <p>{location}</p>
+                    <p>{salary}</p>
                 </div>
                 <div className="card-actions">
                 <button className="btn btn-primary">View Details</button>
